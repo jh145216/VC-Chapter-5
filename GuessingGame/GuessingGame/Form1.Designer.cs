7bef91a42e4884e33baba3d362511dc677875541
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-01-31 13:57:52 -0600

    update guessing

---
+++ GuessingGame/GuessingGame/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace GuessingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.txtNumberGuess = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Pick a number 1 through 10";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOutput.Location = new System.Drawing.Point(43, 92);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(277, 148);
+            this.lblOutput.TabIndex = 8;
+            // 
+            // txtNumberGuess
+            // 
+            this.txtNumberGuess.Location = new System.Drawing.Point(245, 58);
+            this.txtNumberGuess.Name = "txtNumberGuess";
+            this.txtNumberGuess.Size = new System.Drawing.Size(75, 20);
+            this.txtNumberGuess.TabIndex = 7;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(245, 261);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(43, 261);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(75, 23);
+            this.btnGuess.TabIndex = 5;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 310);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtNumberGuess);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnGuess);
+            this.Name = "Form1";
+            this.Text = "Guessing Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox txtNumberGuess;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnGuess;
+    }
+}
+
---
+++ GuessingGame/GuessingGame/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessingGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Random randNumber = new Random();
+            randomNumber = randNumber.Next(1, 10);
+            
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Random randNumber = new Random();
+            randomNumber = randNumber.Next(1, 20);
+            txtNumberGuess.Clear();
+            lblOutput.Text = string.Empty;
+            number = 0;
+            number2 = 0;
+
+        }
+
+        int randomNumber;
+        int number = 0;
+        int number2 = 0;
+
+            
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            string input;
+            int guess;
+
+
+            input = txtNumberGuess.Text;
+            guess = Convert.ToInt32(input);
+
+
+            if (guess != randomNumber)
+            {
+                if (guess > randomNumber)
+                {
+                    number = number + 1;
+                    number2 = 0;
+                    if (number == 2)
+                    {
+
+                        lblOutput.Text = "You dummy! I said you guessed to high!";
+
+                    }
+                    else if (number < 2)
+                    {
+
+                        lblOutput.Text = "You guessed to high. Try again.";
+
+                    }
+
+                }
+                else if (guess < randomNumber)
+                {
+                    number2 = number2 + 1;
+                    number = 0;
+                    if (number2 == 2)
+                    {
+
+                        lblOutput.Text = "You dummy! I said you guessed to low!";
+
+                    }
+                    else if (number2 < 2)
+                    {
+
+                        lblOutput.Text = "You guessed to low. Try again.";
+
+                    }
+
+                }
+            }
+            else
+            {
+                lblOutput.Text = "You guessed it right! the number was " + randomNumber + "!";
+
+            }
+
+        }
+
+    }
+
+
+}
