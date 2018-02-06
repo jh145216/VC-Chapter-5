9f748000c00f2aebee0f1674453ccf88335a2f7e
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-02-06 14:05:45 -0600

    update case 1

---
+++ C5 Case 1/C5 Case 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C5_Case_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********************************");
+            Console.WriteLine("* The stars shine in Greenville *");
+            Console.WriteLine("**********************************");
+            Console.WriteLine("Each contestant pays a $25 entrance fee");
+            Console.WriteLine("The competition is more than twice as big this year!");
+            Console.WriteLine("The competition is bigger than ever!");
+            Console.WriteLine("A tighter race this year! Come out and cast your vote!");
+            Console.WriteLine("Enter a number between 0 and 30");
+            Console.WriteLine("If the user enters an incorrect number, the program prompts for a vaild value");
+        }
+    }
+}
