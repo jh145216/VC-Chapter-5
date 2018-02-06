281cec8da8cbc95899155363f7cd2fbc3b439a48
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-02-06 14:47:14 -0600

    update RPS

---
+++ Rock Paper Scissors GUI/Rock Paper Scissors GUI/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Rock_Paper_Scissors_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblWinLoseDraw = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.picRock = new System.Windows.Forms.PictureBox();
+            this.picPaper = new System.Windows.Forms.PictureBox();
+            this.picScissors = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picRock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPaper)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picScissors)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblWinLoseDraw
+            // 
+            this.lblWinLoseDraw.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblWinLoseDraw.Location = new System.Drawing.Point(43, 271);
+            this.lblWinLoseDraw.Name = "lblWinLoseDraw";
+            this.lblWinLoseDraw.Size = new System.Drawing.Size(82, 37);
+            this.lblWinLoseDraw.TabIndex = 0;
+            // 
+            // lblScore
+            // 
+            this.lblScore.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblScore.Location = new System.Drawing.Point(149, 271);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(88, 37);
+            this.lblScore.TabIndex = 1;
+            // 
+            // picRock
+            // 
+            this.picRock.Image = ((System.Drawing.Image)(resources.GetObject("picRock.Image")));
+            this.picRock.Location = new System.Drawing.Point(43, 49);
+            this.picRock.Name = "picRock";
+            this.picRock.Size = new System.Drawing.Size(116, 76);
+            this.picRock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picRock.TabIndex = 2;
+            this.picRock.TabStop = false;
+            this.picRock.Click += new System.EventHandler(this.picRock_Click);
+            // 
+            // picPaper
+            // 
+            this.picPaper.Image = ((System.Drawing.Image)(resources.GetObject("picPaper.Image")));
+            this.picPaper.Location = new System.Drawing.Point(201, 49);
+            this.picPaper.Name = "picPaper";
+            this.picPaper.Size = new System.Drawing.Size(107, 76);
+            this.picPaper.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPaper.TabIndex = 3;
+            this.picPaper.TabStop = false;
+            this.picPaper.Click += new System.EventHandler(this.picPaper_Click);
+            // 
+            // picScissors
+            // 
+            this.picScissors.Image = ((System.Drawing.Image)(resources.GetObject("picScissors.Image")));
+            this.picScissors.Location = new System.Drawing.Point(342, 49);
+            this.picScissors.Name = "picScissors";
+            this.picScissors.Size = new System.Drawing.Size(126, 76);
+            this.picScissors.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picScissors.TabIndex = 4;
+            this.picScissors.TabStop = false;
+            this.picScissors.Click += new System.EventHandler(this.picScissors_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 332);
+            this.Controls.Add(this.picScissors);
+            this.Controls.Add(this.picPaper);
+            this.Controls.Add(this.picRock);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblWinLoseDraw);
+            this.Name = "Form1";
+            this.Text = "Rock Paper Scissors GUI";
+            ((System.ComponentModel.ISupportInitialize)(this.picRock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPaper)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picScissors)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWinLoseDraw;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.PictureBox picRock;
+        private System.Windows.Forms.PictureBox picPaper;
+        private System.Windows.Forms.PictureBox picScissors;
+    }
+}
+
---
+++ Rock Paper Scissors GUI/Rock Paper Scissors GUI/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rock_Paper_Scissors_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void picRock_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picPaper_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picScissors_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
